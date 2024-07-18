49306831eb910f7b10349be1336dd3f34eeda692
Gio <gioremanente13@gmail.com>
2024-07-19 02:04:03 +0800

    feat: add LoadData method that handles reading to SQL

---
+++ HotelAppLibrary/Databases/SqlDataAccess.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelAppLibrary.Databases
+{
+    public class SqlDataAccess
+    {
+        private readonly IConfiguration _config;
+
+        public SqlDataAccess(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public List<T> LoadData<T, U>(string sqlStatement, U parameters, string connectionStringName)
+        {
+            string connectionString = _config.GetConnectionString(connectionStringName);
+
+            using (IDbConnection conn = new SqlConnection(connectionString))
+            {
+                List<T> rows = conn.Query<T>(sqlStatement,parameters).ToList();
+                return rows;
+            }
+        }
+    }
+}
