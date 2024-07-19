70d55fe9d4e58318838b5d182d4f7c06a1dc63ca
Gio <gioremanente13@gmail.com>
2024-07-20 07:58:54 +0800

    feat: add GuestSearch model

---
+++ HotelApp.Web/Pages/GuestSearch.cshtml.cs
@@ -0,0 +1,44 @@
+using HotelAppLibrary.Data;
+using HotelAppLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelApp.Web.Pages
+{
+    public class GuestSearchModel : PageModel
+    {
+        private readonly IDatabaseData _db;
+
+        public GuestSearchModel(IDatabaseData db)
+        {
+            _db = db;
+        }
+
+        [Display(Name = "Last Name")]
+        [BindProperty(SupportsGet = true)] 
+        public string LastName { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public bool SearchEnabled { get; set; } = false;
+
+        public List<BookingFullModel> Bookings { get; set; }
+
+        public void OnGet()
+        {
+            if (SearchEnabled)
+            {
+                Bookings = _db.SearchBookings(LastName);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            return RedirectToPage(new
+            {
+                SearchEnabled = true,
+                LastName 
+            });
+        }
+    }
+}
