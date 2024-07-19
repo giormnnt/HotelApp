6b565c4980ab572b18259e488cf529a7314545da
Gio <gioremanente13@gmail.com>
2024-07-19 09:22:33 +0800

    feat: add stored procedure spRooms_GetAvailableRooms for retrieving available rooms

---
+++ HotelAppDB/dbo/Stored Procedures/spRooms_GetAvailableRooms.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [dbo].[spRooms_GetAvailableRooms]
+	@startDate date,
+	@endDate date,
+	@roomTypeId int
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	SELECT r.*
+	FROM dbo.Rooms r
+	INNER JOIN dbo.RoomTypes t ON t.Id = r.RoomTypeId
+	WHERE r.RoomTypeId = @roomTypeId
+	AND r.Id NOT IN (
+	SELECT b.RoomId
+	FROM dbo.Bookings b
+	WHERE (@startDate < b.StartDate AND @endDate > b.EndDate)
+		OR (b.StartDate <= @endDate AND @endDate < b.EndDate)
+		OR (b.StartDate <= @startDate AND @startDate < b.EndDate)
+	);
+
+END
+RETURN 0
