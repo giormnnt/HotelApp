c3e145f4aaa8aeb2e59e37f6b1bd3e082ba24bea
Gio <gioremanente13@gmail.com>
2024-07-19 21:55:53 +0800

    feat: add ISqlData interface

---
+++ HotelAppLibrary/Data/ISqlData.cs
@@ -0,0 +1,13 @@
+﻿using HotelAppLibrary.Models;
+
+namespace HotelAppLibrary.Data
+{
+    public interface ISqlData
+    {
+        void BookGuest(string firstName, string lastName, DateTime startDate, DateTime endDate, int roomTypeId);
+        void CheckInGuest(int bookingId);
+        void DeleteBooking(int bookingId);
+        List<RoomTypeModel> GetAvailableRoomTypes(DateTime startDate, DateTime endDate);
+        List<BookingFullModel> SearchBookings(string lastName);
+    }
+}
