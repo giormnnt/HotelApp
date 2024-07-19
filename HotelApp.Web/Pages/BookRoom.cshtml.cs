158e248c5fefed80017d8adf700a6cbcc87eacd3
Gio <gioremanente13@gmail.com>
2024-07-20 04:55:32 +0800

    feat: add BookRoom Model

--- HotelApp.Web/Pages/BookRoom.cshtml.cs
+++ HotelApp.Web/Pages/BookRoom.cshtml.cs
@@ -1,12 +1,55 @@
+using HotelAppLibrary.Data;
+using HotelAppLibrary.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
 namespace HotelApp.Web.Pages
 {
     public class BookRoomModel : PageModel
     {
+        private readonly IDatabaseData _db;
+
+        [BindProperty(SupportsGet = true)]
+        public int RoomTypeId { get; set; }
+
+        private DateTime _startDate = DateTime.Today;
+        private DateTime _endDate = DateTime.Today.AddDays(1);
+
+        [BindProperty(SupportsGet = true)]
+        public DateTime StartDate { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public DateTime EndDate { get; set; }
+
+        [Display(Name = "First Name")]
+        [BindProperty]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [BindProperty]
+        public string LastName { get; set; }
+
+        public RoomTypeModel RoomType { get; set; }
+
+        public BookRoomModel(IDatabaseData db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
+            if (RoomTypeId > 0)
+            {
+                RoomType = _db.GetRoomTypeById(RoomTypeId);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            _db.BookGuest(FirstName, LastName, StartDate, EndDate, RoomTypeId);
+            return RedirectToPage("/Index");
         }
     }
 }
+
