4fdf93754a3584e63ecf35381f7b2accfc4d5915
Gio <gioremanente13@gmail.com>
2024-07-20 00:42:44 +0800

    feat(RoomSearch): Add RoomSearchModel

---
+++ HotelApp.Web/Pages/RoomSearch.cshtml.cs
@@ -0,0 +1,45 @@
+using HotelAppLibrary.Data;
+using HotelAppLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
+
+namespace HotelApp.Web.Pages
+{
+    public class RoomSearchModel : PageModel
+    {
+        private readonly IDatabaseData _db;
+
+        [Display(Name = "Start Date")]
+        [DataType(DataType.Date)]
+        [BindProperty(SupportsGet = true)]
+        public DateTime StartDate { get; set; } = DateTime.Now;
+
+        [Display(Name = "End Date")]
+        [DataType(DataType.Date)]
+        [BindProperty(SupportsGet = true)]
+        public DateTime EndDate { get; set; } = DateTime.Now.AddDays(1);
+
+        [BindProperty(SupportsGet = true)]
+        public bool SearchEnabled { get; set; } = false;
+
+        public RoomSearchModel(IDatabaseData db)
+        {
+            _db = db;
+        }
+
+        public List<RoomTypeModel> AvailableRoomTypes { get; set; }
+        public void OnGet()
+        {
+            if (SearchEnabled)
+            {
+                AvailableRoomTypes = _db.GetAvailableRoomTypes(StartDate, EndDate);
+            }
+        }
+
+        public IActionResult OnPost()
+        {
+            return RedirectToPage(new { SearchEnabled = true, StartDate, EndDate });
+        }
+    }
+}
